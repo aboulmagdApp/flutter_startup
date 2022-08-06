09797a5a93b6097b3be821661417739783414baa
aboulmagd <aboulmagd@live.com>
2022-08-06 15:28:47 +0300

    Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,24 +31,26 @@ class Login extends StatelessWidget {
         appBar: AppBar(),
         drawer: Drawer(),
         body: Container(
-          width: double.infinity,
-          height: 400,
-          color: Colors.red,
-          child: Column(
-            // mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // x محور
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("How Are You"),
-              Text("How Are You Too"),
-              Container(
-                color: Colors.red,
-                child: Text("this is Flutter "),
-              ),
-              // Image.asset('assets/images/unnamed.png')
-            ],
-          ),
-        ));
+            width: double.infinity,
+            height: 400,
+            color: Colors.red,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("text "),
+                Text("text two "),
+                Container(
+                  color: Colors.green,
+                  child: Text("Container"),
+                ),
+                Card(
+                  child: Text("Card"),
+                ),
+                Card(
+                  child: Text("Card two"),
+                )
+              ],
+            )));
   }
 }

1c8576ff262fce8d2a7379b4c7604edf32a7bf2e
aboulmagd <aboulmagd@live.com>
2022-08-06 15:39:29 +0300

    stack part 1

--- lib/main.dart
+++ lib/main.dart
@@ -30,27 +30,34 @@ class Login extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(),
         drawer: Drawer(),
-        body: Container(
-            width: double.infinity,
-            height: 400,
-            color: Colors.red,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("text "),
-                Text("text two "),
-                Container(
-                  color: Colors.green,
-                  child: Text("Container"),
-                ),
-                Card(
-                  child: Text("Card"),
-                ),
-                Card(
-                  child: Text("Card two"),
-                )
-              ],
-            )));
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.red,
+              width: 400,
+              height: 400,
+            ),
+            Container(
+              color: Colors.green,
+              width: 300,
+              height: 300,
+              child: Text("widget two"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              color: Colors.blue,
+              width: 200,
+              height: 200,
+              child: Text("widget three"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              color: Colors.brown,
+              width: 200,
+              height: 200,
+              child: Text("widget four"),
+            ),
+          ],
+        ));
   }
 }

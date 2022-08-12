8b4b9c9b7939654fceacc8db74c50ac4d94d24c5
aboulmagd <aboulmagd@live.com>
2022-08-12 14:23:01 +0300

    Radio

--- lib/test.dart
+++ lib/test.dart
@@ -11,6 +11,7 @@ class TestState extends State<Test> {
   bool usa = false;
   bool sa = false;
   bool eg = false;
+  String country = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,58 +24,49 @@ class TestState extends State<Test> {
               "Choose Country",
               style: TextStyle(fontSize: 30),
             ),
-            CheckboxListTile(
-                controlAffinity: ListTileControlAffinity.leading,
-                activeColor: Colors.green,
-                contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                title: Text("country USA"),
-                subtitle: Text(
-                  "America",
-                  style: TextStyle(color: Colors.red),
-                ),
-                secondary: Icon(Icons.flag),
-                isThreeLine: true,
-                selected: sa,
-                value: sa,
-                onChanged: (val) {
-                  setState(() {
-                    sa = val!;
-                  });
-                }),
-            CheckboxListTile(
-                activeColor: Colors.green,
-                contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                title: Text("country KSA"),
-                subtitle: Text(
-                  "Riyadh",
-                  style: TextStyle(color: Colors.red),
-                ),
-                secondary: Icon(Icons.flag),
-                isThreeLine: true,
-                selected: usa,
-                value: usa,
-                onChanged: (val) {
-                  setState(() {
-                    usa = val!;
-                  });
-                }),
-            CheckboxListTile(
-                activeColor: Colors.green,
-                contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                title: Text("country Egypt"),
-                subtitle: Text(
-                  "Cairo",
-                  style: TextStyle(color: Colors.red),
-                ),
-                secondary: Icon(Icons.flag),
-                isThreeLine: true,
-                selected: eg,
-                value: eg,
-                onChanged: (val) {
-                  setState(() {
-                    eg = val!;
-                  });
-                }),
+            Row(
+              children: [
+                Text("Egypt"),
+                Radio(
+                    value: "eg",
+                    groupValue: country,
+                    onChanged: (String? val) {
+                      setState(() {
+                        country = val!;
+                        print(country);
+                      });
+                    })
+              ],
+            ),
+            Row(
+              children: [
+                Text("USA"),
+                Radio(
+                    value: "usa",
+                    groupValue: country,
+                    onChanged: (String? val) {
+                      setState(() {
+                        country = val!;
+                        print(country);
+                      });
+                    })
+              ],
+            ),
+            Row(
+              children: [
+                Text("Sudia"),
+                Radio(
+                    activeColor: Colors.red,
+                    value: "sa",
+                    groupValue: country,
+                    onChanged: (String? val) {
+                      setState(() {
+                        country = val!;
+                        print(country);
+                      });
+                    })
+              ],
+            ),
           ]),
         ));
   }

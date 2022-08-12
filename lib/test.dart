2352fd5e2f5d353e91da88688ca142b3aac023f5
aboulmagd <aboulmagd@live.com>
2022-08-12 14:41:28 +0300

    Switch

--- lib/test.dart
+++ lib/test.dart
@@ -8,54 +8,32 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State<Test> {
-  String country = "";
+  bool notify = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         drawer: Drawer(),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(children: [
-            Text(
-              "Choose Country",
-              style: TextStyle(fontSize: 30),
-            ),
-            RadioListTile(
-                title: Text('Country Egypt'),
-                subtitle: Text('Cairo'),
-                value: "Eg",
-                groupValue: country,
-                onChanged: (String? val) {
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('هل تريد تفعيل الاشعارات'),
+              Switch(
+                activeColor: Colors.pink,
+                activeTrackColor: Colors.amber,
+                inactiveTrackColor: Colors.black,
+                inactiveThumbColor: Colors.green,
+                value: notify,
+                onChanged: (val) {
                   setState(() {
-                    country = val!;
+                    notify = val;
+                    print(notify);
                   });
-                }),
-            RadioListTile(
-                title: Text('Country USA'),
-                subtitle: Text('America'),
-                value: "USA",
-                groupValue: country,
-                onChanged: (String? val) {
-                  setState(() {
-                    country = val!;
-                  });
-                }),
-            RadioListTile(
-                controlAffinity: ListTileControlAffinity.leading,
-                selected: country == "em" ? true : false,
-                activeColor: Colors.brown,
-                title: Text('Country UAE'),
-                subtitle: Text('Ematate'),
-                secondary: Icon(Icons.flag),
-                value: "em",
-                groupValue: country,
-                onChanged: (String? val) {
-                  setState(() {
-                    country = val!;
-                  });
-                }),
-          ]),
+                },
+              )
+            ],
+          ),
         ));
   }
 }

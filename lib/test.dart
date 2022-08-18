0ece324f88db5e3538d8a57246dfdcd7c966e134
aboulmagd <aboulmagd@live.com>
2022-08-18 03:47:29 +0300

    TabBarView

--- lib/test.dart
+++ lib/test.dart
@@ -9,74 +9,25 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State<Test> {
-  List mobiles = [
-    {"name": "s20 Ultra", "screen": "6.2", "cpu": "8 core"},
-    {"name": "s21 Ultra", "screen": "6.3", "cpu": "5 core"},
-    {"name": "s10", "screen": "7.3", "cpu": "2 core"},
-    {"name": "iphone 13 proMax", "screen": "7.3", "cpu": "10 core"}
-  ];
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> _scaffoldKey =
-        new GlobalKey<ScaffoldState>();
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Homepage'),
-          leading: Icon(Icons.home),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.save)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.alarm_add)),
-          ],
-          elevation: 2,
-          shadowColor: Colors.blue,
-          backgroundColor: Colors.brown,
-          brightness: Brightness.light,
-          centerTitle: true,
-        ),
-        drawerScrimColor: Colors.red.withOpacity(0.8),
-        drawer: Drawer(
-          child: Column(children: [
-            UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: Text(
-                    'M',
-                    style: TextStyle(color: Colors.yellow),
-                  ),
-                ),
-                accountName: Text('aboulmagd'),
-                accountEmail: Text('aboulmagd"live.com')),
-            ListTile(
-              title: Text('Home page'),
-              leading: Icon(Icons.home),
-              onTap: () {},
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Homepage'),
             ),
-            ListTile(
-              title: Text('Help'),
-              leading: Icon(Icons.help),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('About'),
-              leading: Icon(Icons.help_center),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('Logout'),
-              leading: Icon(Icons.logout),
-              onTap: () {},
-            ),
-          ]),
-        ),
-        body: Center(
-          child: Container(
-            child: Builder(builder: (context) {
-              return RaisedButton(
-                onPressed: () => Scaffold.of(context).openDrawer(),
-                child: Text('open drawer'),
-              );
-            }),
-          ),
-        ));
+            body: TabBarView(
+              children: [
+                Container(
+                    color: Colors.green,
+                    width: double.infinity,
+                    child: Text('Mohamed')),
+                Container(
+                    color: Colors.red,
+                    width: double.infinity,
+                    child: Text('aboulamgd')),
+              ],
+            )));
   }
 }

c5983bc9d445911e8bd8544408c5ed89bbaca4b0
aboulmagd <aboulmagd@live.com>
2022-08-20 11:01:53 +0300

    textinput prop

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -7,47 +7,25 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  late PageController pc;
-  int selectedIndex = 0;
-  @override
-  void initState() {
-    pc = new PageController(initialPage: 0, viewportFraction: 0.6);
-    super.initState();
-  }
-
-  List images = [
-    {"url": "images/fb.png"},
-    {"url": "images/flutter.png"},
-    {"url": "images/WhatsApp.png"},
-    {"url": "images/google.png"},
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Homepage'),
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 300,
-              child: PageView.builder(
-                controller: pc,
-                itemCount: images.length,
-                itemBuilder: (context, index) {
-                  return Image.asset(images[index]['url']);
-                },
-              ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                pc.animateToPage(2,
-                    duration: Duration(seconds: 1), curve: Curves.easeIn);
-              },
-              child: Text('go TO Page 2'),
-            )
-          ],
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    // hintText: "ادخل الاسم",
+                    // hintStyle: TextStyle(color: Colors.blue),
+                    // prefixIcon: Icon(Icons.search)
+                    suffix: Text('ادخل الاسم')),
+              )
+            ],
+          ),
         ));
   }
 }

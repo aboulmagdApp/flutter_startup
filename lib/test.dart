8417af6026d2a484b1e4e4cf02066a6ed9cf31a4
aboulmagd <aboulmagd@live.com>
2022-08-23 04:14:13 +0300

    showModalBottomSheet

--- lib/test.dart
+++ lib/test.dart
@@ -9,52 +9,26 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  late ScrollController sc;
-
-  @override
-  void initState() {
-    sc = new ScrollController();
-    sc.addListener(() {
-      print(sc.offset);
-      print("Max scroll : ${sc.position.maxScrollExtent}");
-      print("Min scroll : ${sc.position.minScrollExtent}");
-    });
-
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Text page'),
         ),
-        body: ListView(
-          controller: sc,
-          children: [
-            RaisedButton(
-              onPressed: () {
-                sc.jumpTo(sc.position.maxScrollExtent);
-              },
-              child: Text("jump to bottom"),
-            ),
-            ...List.generate(
-                8,
-                (index) => Container(
-                      margin: EdgeInsets.all(10),
-                      color: index.isEven ? Colors.green : Colors.red,
-                      height: 100,
-                      width: double.infinity,
-                      child: Text('Container $index'),
-                    )),
-            RaisedButton(
-              onPressed: () {
-                sc.animateTo(0,
-                    duration: Duration(seconds: 1), curve: Curves.easeIn);
-              },
-              child: Text("jump to top"),
-            )
-          ],
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      child: Text("bottomSheet"),
+                      height: 200,
+                    );
+                  });
+            },
+            child: Text("Show Modal Bottom Sheet"),
+          ),
         ));
   }
 }

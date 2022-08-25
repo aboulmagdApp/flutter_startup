396f17203fc7cab9e54e4ef4ccdd886d58cb03e9
aboulmagd <aboulmagd@live.com>
2022-08-25 23:06:21 +0300

    update geocodeing

--- lib/widgets/geolocator.dart
+++ lib/widgets/geolocator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 // we will add the permition for Android in the following path :-
 // - android -> app -> src -> main -> AndroidManifest.xml
@@ -16,12 +17,60 @@ class GeolocatorTest extends StatefulWidget {
 }
 
 class _GeolocatorTestState extends State<GeolocatorTest> {
+  // services Location
+  var cl;
+  Future getPosition() async {
+    bool services;
+    LocationPermission per;
+
+    services = await Geolocator.isLocationServiceEnabled();
+    if (services == false) {
+      AwesomeDialog(
+              context: context,
+              title: "services",
+              body: Text('services not enable'))
+          .show();
+    }
+    print("location is ${services} in your device");
+
+    per = await Geolocator.checkPermission();
+    if (per == LocationPermission.denied) {
+      per = await Geolocator.requestPermission();
+      if (per == LocationPermission.whileInUse) {
+        getLatAndLong();
+      }
+    }
+
+    print("permition ${per} in your device");
+  }
+
+  Future<Position> getLatAndLong() async {
+    return await Geolocator.getCurrentPosition().then((value) => value);
+  }
+
+  @override
+  void initState() {
+    getPosition();
+    super.initState();
+  }
+
+  // permission
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Geolocator'),
         ),
-        body: Text('Geolocator'));
+        body: Center(
+          child: RaisedButton(
+            onPressed: () async {
+              cl = await getLatAndLong();
+              print(cl);
+              print(cl.latitude);
+              print(cl.longitude);
+            },
+            child: Text('show lat and long'),
+          ),
+        ));
   }
 }

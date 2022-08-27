e23a65eb651f94f2ff05c0555e934ac53f701dfe
aboulmagd <aboulmagd@live.com>
2022-08-27 08:02:38 +0300

    add to google

--- lib/widgets/googlemap.dart
+++ lib/widgets/googlemap.dart
@@ -12,8 +12,8 @@ class GoogleMapTest extends StatefulWidget {
 
 Completer<GoogleMapController> _controller = Completer();
 final CameraPosition _kGooglePlex = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
-  zoom: 14.4746,
+  target: LatLng(24.696434, 46.673228),
+  zoom: 8,
 );
 
 class _GoogleMapTestState extends State<GoogleMapTest> {

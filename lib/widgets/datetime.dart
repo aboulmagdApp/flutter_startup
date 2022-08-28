cd7cd260d8d2ec836aa5bcc861767a049925fd07
aboulmagd <aboulmagd@live.com>
2022-08-28 04:08:43 +0300

    Jiffy

--- lib/widgets/datetime.dart
+++ lib/widgets/datetime.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 
 class DatetimeTest extends StatefulWidget {
   const DatetimeTest({super.key});
@@ -8,19 +9,35 @@ class DatetimeTest extends StatefulWidget {
 }
 
 class _DatetimeTestState extends State<DatetimeTest> {
-  DateTime dateOne = DateTime.now();
+  DateTime date = DateTime.now().subtract(Duration(hours: 3));
   DateTime datetwo = DateTime.now().add(Duration(days: 10));
   DateTime datethree = DateTime.now().subtract(Duration(days: 10));
+
+  changeLocal() async {
+    await Jiffy.locale("ar");
+  }
+
+  @override
+  void initState() {
+    changeLocal();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      child: RaisedButton(
-        onPressed: () {
-          print(dateOne.isBefore(datethree));
-        },
-        child: Text('Show Date Time'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('datetime'),
       ),
-    ));
+      body: Center(
+          child: Container(
+        child: RaisedButton(
+          onPressed: () {
+            print(Jiffy(date).fromNow());
+          },
+          child: Text('Show Date Time'),
+        ),
+      )),
+    );
   }
 }
